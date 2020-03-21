9118784e2d0535c4406f59621d334c86f79031ba
Nozomi Ishii <non.ankt@gmail.com>
2020-03-22 02:31:29 +0900

    create 404 page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,19 @@
 import React from "react"
+import headerStyles from "../styles/components/header.module.scss"
+import { Link } from "gatsby"
 
-const Header = () => <div>Hashiya</div>
+const Header = () => (
+  <div className={headerStyles.header}>
+    <Link to="/" activeClassName={headerStyles.activeNavItem}>
+      ホーム
+    </Link>
+    <Link to="/blog" activeClassName={headerStyles.activeNavItem}>
+      ブログ
+    </Link>
+    <Link to="/contact" activeClassName={headerStyles.activeNavItem}>
+      お問い合わせ
+    </Link>
+  </div>
+)
 
 export default Header
---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Head from "../components/head"
+import Layout from "../components/Layout"
+
+const NotFoundPage = () => (
+  <Layout>
+    <Head title="404" />
+    <div>404</div>
+  </Layout>
+)
+
+export default NotFoundPage
