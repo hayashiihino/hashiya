50f17c1f30fdc174f4769c9b5bc1d3fdfdfbff4a
Nozomi Ishii <non.ankt@gmail.com>
2020-03-24 18:25:32 +0900

    footer on the bottom

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,13 +4,16 @@ import Footer from "./Footer";
 import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/base/theme.scss";
+import LayoutStyles from "../styles/components/layout.module.scss";
 
 const Layout = props => (
-  <React.Fragment>
-    <Header />
-    <Container className="my-5">{props.children}</Container>
+  <div className={LayoutStyles.container}>
+    <div className={LayoutStyles.content}>
+      <Header />
+      <Container className="my-5">{props.children}</Container>
+    </div>
     <Footer />
-  </React.Fragment>
+  </div>
 );
 
 export default Layout;
