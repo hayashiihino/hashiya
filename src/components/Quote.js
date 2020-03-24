aaca49f6d7ccdeddb971e94e43fa3e9282721334
Nozomi Ishii <non.ankt@gmail.com>
2020-03-24 20:14:33 +0900

    change file

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,9 +1,18 @@
 import React from "react";
 
+function randomQuote() {
+  let randomNum = Math.floor(Math.random() * 3);
+  console.log(randomNum);
+  // let quote = `${__dirname}images/quote/q${randomNum}.jpg`;
+  return <img src="../images/quote/q0.jpg" alt="quote" />;
+}
+
 export default function Quote() {
+  const TodaysQuote = randomQuote();
   return (
     <div>
       <h3>今日の名言</h3>
+      {TodaysQuote}
     </div>
   );
 }
