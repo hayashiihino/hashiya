fdac8926229af4967b6a3c1e8a77d40ac06ab56b
Nozomi Ishii <non.ankt@gmail.com>
2020-03-29 21:44:24 +0900

    create illustration page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,8 @@
-import React from "react"
-import Head from "../components/Head"
-import Layout from "../components/Layout"
-import { useStaticQuery, graphql, Link } from "gatsby"
+import React from "react";
+import Head from "../components/Head";
+import Layout from "../components/Layout";
+import { useStaticQuery, graphql, Link } from "gatsby";
+import classes from "../styles/pages/blog.module.scss";
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
@@ -9,33 +10,46 @@ const BlogPage = () => {
       allContentfulBlog(sort: { fields: publishedDate, order: DESC }) {
         edges {
           node {
+            id
             title
             slug
             publishedDate(formatString: "MMMM Do, YYYY")
+            thumbnail {
+              fluid {
+                src
+              }
+            }
           }
         }
       }
     }
-  `)
+  `);
 
   return (
     <Layout>
       <Head title="ブログ" />
       <h1>ブログページ</h1>
-      <ol>
+      <ol className={classes.posts}>
         {data.allContentfulBlog.edges.map(edge => {
           return (
-            <li key={edge.node.title}>
+            <li key={edge.node.id} className={classes.post}>
               <Link to={`/blog/${edge.node.slug}`}>
-                <h3>{edge.node.title}</h3>
-                <p>{edge.node.date}</p>
+                <img
+                  src={edge.node.thumbnail.fluid.src}
+                  alt={edge.node.title}
+                  className="pr-3"
+                />
+                <div className={classes.content}>
+                  <h3>{edge.node.title}</h3>
+                  <p>{edge.node.publishedDate}</p>
+                </div>
               </Link>
             </li>
-          )
+          );
         })}
       </ol>
     </Layout>
-  )
-}
+  );
+};
 
-export default BlogPage
+export default BlogPage;
--- src/pages/illustration.js
+++ src/pages/illustration.js
@@ -1,12 +1,48 @@
 import React from "react";
 import Head from "../components/Head";
 import Layout from "../components/Layout";
+import { useStaticQuery, graphql } from "gatsby";
+// import Img from "gatsby-image";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+// import classes from "../styles/pages/illustration.module.scss";
 
-const Illustration = () => (
-  <Layout>
-    <Head title="イラスト" />
-    <h2>イラスト</h2>
-  </Layout>
-);
+const Illustration = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulIllustration {
+        edges {
+          node {
+            title
+            id
+            illustration {
+              fluid {
+                src
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const Illustrationlist = () => {
+    const source = data.allContentfulIllustration.edges;
+    return source.map(image => (
+      <Col md={3} key={image.node.id}>
+        <img src={image.node.illustration.fluid.src} alt={image.node.title} />
+        <p>{image.node.title}</p>
+      </Col>
+    ));
+  };
+
+  return (
+    <Layout>
+      <Head title="イラスト" />
+      <h2>イラスト</h2>
+      <Row>{Illustrationlist()}</Row>
+    </Layout>
+  );
+};
 
 export default Illustration;
